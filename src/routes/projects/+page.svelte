883303d73eda6374bc49217939f4d7eae94a4148
medianon <medianon8459@gmail.com>
2025-05-21 13:17:06 +0100

    update link

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -1,18 +1,19 @@
 <div class="p-20">
     <h5>
-        <a href="https://github.com/sid-c1/API-examples" target="_blank">
+        <a href="https://geonamesexample.free.nf" target="_blank">
             GeoNames API Examples
         </a>
     </h5>
     <p>
         I set up a table to get the required inputs from the user. <br>
         A second table displays the returned data. <br><br>
+        <a href="https://github.com/sid-c1/API-examples">GitHub link</a><br><br>
         Uses: <br>
         <a href="https://www.geonames.org">GeoNames</a> <br>
         HTML, JS, PHP
     </p> <br><br>
     <h5>
-        <a href="https://github.com/medianon/medianon" target="_blank">
+        <a href="https://medianon.org" target="_blank">
             medianon
         </a>
     </h5>
@@ -21,7 +22,7 @@
         Users can make a custom board or use a public board. <br>
         Posts are automatically archived and deleted. <br>
         No registration, cookies or tracking. <br><br>
-        <a href="https://medianon.org" target="_blank">medianon</a> <br><br>
+        <a href="https://github.com/medianon/medianon" target="_blank">GitHub link</a> <br><br>
         Uses: <br>
         SvelteKit, Tailwind, Firebase, Vercel
 
